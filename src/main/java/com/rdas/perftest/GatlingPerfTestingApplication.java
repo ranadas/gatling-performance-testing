23c8b03dc95cbb50295537038c890898bb5f1f2b
Rana Das <rana.pratap.das@gmail.com>
2018-07-21 08:25:05 +0100

    first commit with sboot main

---
+++ src/main/java/com/rdas/perftest/GatlingPerfTestingApplication.java
@@ -0,0 +1,12 @@
+package com.rdas.perftest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatlingPerfTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatlingPerfTestingApplication.class, args);
+    }
+}
---
+++ src/test/java/com/rdas/perftest/GatlingPerfTestingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rdas.perftest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GatlingPerfTestingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
